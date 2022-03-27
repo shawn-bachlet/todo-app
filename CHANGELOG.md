a21a7b2c2683d452830b253bebfedfbcebbd3fa2
sbachlet <sbachlet@gmail.com>
2022-03-27 14:54:54 -0400

    Basic nix project

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# Revision history for todo-app
+
+## 0.1.0.0 -- YYYY-mm-dd
+
+* First version. Released on an unsuspecting world.
+* Add basic cabal project
+* Add a basic build with haskell.nix and niv
---
+++ default.nix
@@ -0,0 +1,11 @@
+let
+  sources = import ./nix/sources.nix {};
+  haskellNix = import sources.haskellNix {};
+  pkgs = import haskellNix.sources.nixpkgs-unstable haskellNix.nixpkgsArgs;
+in pkgs.haskell-nix.project {
+  src = pkgs.haskell-nix.haskellLib.cleanGit {
+    name = "todo-app";
+    src = ./.;
+  };
+  compiler-nix-name = "ghc8107";
+}
